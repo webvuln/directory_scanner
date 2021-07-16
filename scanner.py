11f99e7aec7ddedcd80c079e80596b3f21d3ff95
anonymouse327311 <66708163+anonymouse327311@users.noreply.github.com>
2021-07-16 02:02:36 -0400

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -5,7 +5,7 @@ T = input("update or scanning?: ")
 if T == "update":
     os.system('sudo apt-get update')
     os.system('sudo apt install dirb')
-    os.system('github link here ')
+    os.system('git clone https://github.com/anonymouse327311/directory_scanner')
 
 if T == "scanning":
     Web = input("URL of website with http: ")
