514cbf44422aee3eccb48f5f00a35493d8c435eb
webvuln <66708163+webvuln@users.noreply.github.com>
2022-01-31 08:23:24 -0500

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,12 +1,13 @@
 import os
 
-T = input("update or scanning?: ")
+def main():
+    T = input("update or scanning?: ")
 
-if T == "update":
-    os.system('sudo apt-get update')
-    os.system('sudo apt install dirb')
-    os.system('git clone https://github.com/anonymouse327311/directory_scanner')
+    if T == "update":
+        os.system('sudo apt-get update')
+        os.system('sudo apt install dirb')
+        os.system('git clone https://github.com/anonymouse327311/directory_scanner')
 
-if T == "scanning":
-    Web = input("URL of website with http: ")
-    os.system('dirb ' + Web + ' -w >> results.txt')
+    if T == "scanning":
+        Web = input("URL of website with http: ")
+        os.system('dirb ' + Web + ' -w >> results.txt')
